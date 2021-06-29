500c608628e26d4182eb4cd59f4dec7f03e1f395
Snobrost <=>
2021-06-29 15:44:39 +0200

    ajout dons

---
+++ Ipssi-Api-master/App/Entity/Don.php
@@ -0,0 +1,73 @@
+<?php
+namespace App\Entity;
+
+class Animal {
+
+    public $id;
+    public $pseudo,
+    public $montant,
+    public $date
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of pseudo
+     */
+    public function getPseudo()
+    {
+        return $this->pseudo;
+    }
+
+    /**
+     * Set the value of pseudo
+     */
+    public function setPseudo($pseudo): self
+    {
+        $this->pseudo = $pseudo;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of montant
+     */
+    public function getMontant()
+    {
+        return $this->montant;
+    }
+
+    /**
+     * Set the value of montant
+     */
+    public function setMontant($montant): self
+    {
+        $this->montant = $montant;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of date
+     */
+    public function getDate)
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set the value of date
+     */
+    public function setDate($date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+}
